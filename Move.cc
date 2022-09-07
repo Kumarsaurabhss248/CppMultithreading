a3109396a7d0203fdd3d99fbc297c156683e8836
Kumar Saurabh <kumarsaurabhss248@gmail.com>
2022-09-07 19:01:28 +0530

    Create Move.cc

---
+++ Move.cc
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<thread>
+#include<string>
+using namespace std;
+
+/* By using move we can move the whole string to the caller argument and the ownership goes to the caller function from the main */
+void func(string && s){ //Rvalue Reference
+
+  cout<<"Ownership "<<s<<" transfered to thread "<<endl;
+}
+int main(){
+      string s="abc";
+      thread t{func, std::move(s)}; // Passing a value by move 
+
+      t.join(); //Tells the main fucnction to stop til this thread runs
+
+      cout<<"String is now "<<s<<endl; // If we try to access string it shows nothing as ownership not lies to main func
+
+      return 0;
+}
+
+O/P:
+Ownership abc transfered to thread 
+String is now
