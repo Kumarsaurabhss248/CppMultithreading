3c1ab50d7375d131e3bba58e399d9193ce5f4bba
Kumar Saurabh <kumarsaurabhss248@gmail.com>
2022-09-07 18:20:42 +0530

    Update hellothread.cc

--- hellothread.cc
+++ hellothread.cc
@@ -10,6 +10,10 @@ int main(){
       thread t{ hello }; // Calling a thread
       cout<<"Doing some work"<<endl;
        t.join(); //Tells the main fucnction to stop til this thread runs
-
+/* When more than one thread runnings at same time there action can "interleave" */ 
       return 0;
 }
+
+o/p:
+Doing some work
+Hello Thread
