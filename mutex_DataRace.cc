10d94c0e0106f8bf139deafd15602483da7b1446
Kumar Saurabh <kumarsaurabhss248@gmail.com>
2022-09-09 01:38:18 +0530

    Create mutex_DataRace.cc

---
+++ mutex_DataRace.cc
@@ -0,0 +1,46 @@
+//Adding  mutex solves the problem of data inconsistency in this case
+#include<iostream>
+#include<thread>
+#include<chrono>
+#include<string>
+#include<mutex>
+
+using namespace std;
+using namespace std::literals;
+
+mutex print_mutex;
+void print(string str)
+{
+    for(int i=0;i<5;++i)
+    {
+        print_mutex.lock();
+        cout<< str[0]<<str[1]<<str[2]<<endl;
+        print_mutex.unlock();
+        std::this_thread::sleep_for(50ms);
+    }
+}
+int main(){
+     thread t1{ print,"abc"};
+     thread t2{ print,"def"};
+     thread t3{ print,"xyz"};
+     t1.join();
+     t2.join();
+     t3.join();
+    return 0;
+}
+O/P:
+def
+xyz
+abc
+abc
+def
+xyz
+abc
+xyz
+def
+def
+abc
+xyz
+abc
+def
+xyz
