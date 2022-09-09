056a2e12fce5e798da3876d8c1347c48d7738621
Kumar Saurabh <kumarsaurabhss248@gmail.com>
2022-09-09 21:11:19 +0530

    Update and rename SimpleMultithreadingByClassObject.cc to LaunchingAThread_4.cc

--- LaunchingAThread_4.cc
+++ LaunchingAThread_4.cc
@@ -16,7 +16,7 @@ int main(){
       greeter greet;
       thread t1{ &greeter::fun, &greet };// Calling a class function and passing an object address 
       cout<<"t1 and t2 threads called\n";   
-      thread t2{&greeter::hello, &greet};
+      thread t2{&greeter::hello, &greet}; /// Create an instance of std::thread and initialize it with the task
       t1.join(); //Tells the main fucnction to stop til this thread runs
       t2.join();
       cout<<"Both t1 and t2 threads are finished now main thread can stop";
